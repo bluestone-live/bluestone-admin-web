8d74b5f44a8fde56443f8418b64171ee5a35ae1b
CanvasL <746591811@qq.com>
2022-09-28 11:20:44 +0800

    feat: init configuration page

---
+++ src/components/loading/ConfigurationLoading.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="loading-spinner">
+    <fingerprint-spinner
+      :animation-duration="1000"
+      :size="300"
+      :color="'#6F92DA'"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/runtime-core";
+import { FingerprintSpinner } from "epic-spinners";
+export default defineComponent({
+  name: "ConfigurationLoading",
+  components: {
+    FingerprintSpinner,
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped>
+.loading-spinner {
+  display: flex;
+  height: 80vh;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ src/pages/admin/configuration/Configuration.vue
@@ -0,0 +1,26 @@
+<template>
+    <Suspense>
+      <template #fallback>
+        <configuration-loading />
+      </template>
+      <configuration-detail />
+    </Suspense>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import ConfigurationDetail from "./ConfigurationDetail.vue";
+import ConfigurationLoading from "@/components/loading/ConfigurationLoading.vue"
+
+export default defineComponent({
+  name: "Configuration",
+  components: {
+    ConfigurationDetail,
+    ConfigurationLoading,
+  },
+  setup() {},
+});
+</script>
+
+<style lang="scss" scoped>
+</style>
