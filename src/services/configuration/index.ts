2f84aa8c366acb7d759f163ea2bd87fd57f96681
CanvasL <746591811@qq.com>
2022-10-22 14:57:46 +0800

    feat: add configuration services

--- src/services/configuration/index.ts
+++ src/services/configuration/index.ts
@@ -1,41 +1,45 @@
 import { reactive } from "vue";
 import * as echarts from "echarts";
+import { BigNumber, ethers } from "ethers";
+import utils from "@/utils";
+import { TokenType } from "../types";
 
-export const useConfiguration = (whitelistStore: any) => {
+export const useConfiguration = async (commonStore: any, whitelistStore: any, pendingStore: any) => {
     const state = reactive({
         administrators: whitelistStore.administrators,
-        openAddGatewayAddress: false,
-        openAddInterestModel: false,
-        openSetMinCCR: false,
+        openSetGatewayAddress: false,
+        openSetInterestRateModel: false,
+        openSetMinCollateralCoverageRatio: false,
         currentGatewayAddress: "0x9489f551e2dd40aA69518D2502Fe394212A0020D",
-        gatewayAddressInProposal: "0x13a6D1fe418de7e5B03Fb4a15352DfeA3249eAA4",
-        currentMinCCR: 150,
-        minCCRInProposal: 130,
-        loanParams: {
-            termList: [1, 30, 60, 120, 240],
-            interestList: [6.5, 8, 10, 11, 13.5],
-        }
+        currentMinCollateralCoverageRatio: {
+            ETH: 0,
+            xBTC: 0,
+        },
+        currentInterestRateParams: {
+            termList: [],
+            interestRateList: [],
+        },
+        chartNeedRefresh: false,
+        inputTermList: "",
+        inputInterestRateList: "",
+        inputGatewayAddress: "",
+        inputMinCollateralCoverageRatio: "",
+        isSetInterestRateModelLoading: false,
+        isSetGatewayAddressLoading: false,
+        isSetMinCollateralCoverageRatioLoading: false,
     })
 
-    const initChartOption = (termList: Array<number>, interestList: Array<number>) => {
-        const daysInYear = Array.from({ length: 365 }, (_, i) => i + 1);
-        const sgcInterest = [];
-        let paramIndex = 0;
-        for (let term of daysInYear) {
-            if (term >= termList[paramIndex]) {
-                if (
-                    paramIndex < termList.length - 1 &&
-                    term == termList[paramIndex + 1]
-                ) {
-                    paramIndex++;
-                }
-                sgcInterest.push(interestList[paramIndex]);
-            }
-        }
+    await Promise.all([
+        initInterestRateModelParams(),
+        initCurrentMinCollateralRatio(),
+    ])
 
-        return {
+    const initChart = () => {
+        const chartDom = document.getElementById("interest-model-chart");
+        const myChart = echarts.init(chartDom!);
+        myChart.setOption({
             title: {
-                text: "Loan Inerest(%)",
+                text: "Mapping Interest Rate Model",
             },
             tooltip: {
                 trigger: "axis",
@@ -56,7 +60,11 @@ export const useConfiguration = (whitelistStore: any) => {
             },
             xAxis: {
                 type: "category",
-                data: daysInYear,
+                data: state.currentInterestRateParams.termList,
+                axisLabel: {
+                    label: "Loan Term",
+                    color: 'black',
+                }
             },
             yAxis: {
                 type: "value",
@@ -64,23 +72,169 @@ export const useConfiguration = (whitelistStore: any) => {
             series: [
                 {
                     name: "SGC",
-                    type: "line",
+                    type: "scatter",
                     step: "start",
-                    data: sgcInterest,
+                    data: state.currentInterestRateParams.interestRateList,
+                    itemStyle: {
+                        normal: {
+                            label: {
+                                show: true,
+                                position: 'top',
+                                textStyle: {
+                                    color: 'black',
+                                    fontSize: 10
+                                },
+                            }
+                        }
+                    },
                 },
             ],
+        });
+    }
+
+    const setInterestRateParameters = async () => {
+        const terms = state.inputTermList.toString().replace(/\s*/g,"").split(",").map((term) => BigNumber.from(term));
+        const interestRates = state.inputInterestRateList.toString().replace(/\s*/g,"").split(",").map((interestRate) => ethers.utils.parseUnits(interestRate, "ether"));
+        let tx, result;
+        try {
+            tx = await commonStore.getInterestRateModel.setLoanParameters(
+                commonStore.getTokens.SGC.address,
+                terms,
+                interestRates,
+            );
+        } catch (error) {
+            console.error(error);
+            pendingStore.enqueue({
+                title: "Configuration",
+                message: `Set parameters of InterestRateModel failed. (${utils.filterRevertMsg((error as any).message)})`,
+                color: "danger"
+            });
+            return;
+        }
+        try {
+            state.isSetInterestRateModelLoading = true;
+            pendingStore.increment();
+            result = await tx.wait();
+            console.log("setInterestRateParameters result: ", result);
+            pendingStore.enqueue({
+                title: "Configuration",
+                message: `Set InterestRateParameters [${state.inputTermList}] => [${state.inputInterestRateList}] success.`,
+                color: "success"
+            });
+            state.isSetInterestRateModelLoading = false;
+            pendingStore.decrement();
+        } catch (error) {
+            console.error(error);
+            pendingStore.enqueue({
+                title: "Configuration",
+                message: `Set parameters of InterestRateModel failed. (${utils.filterRevertMsg((error as any).message)})`,
+                color: "danger"
+            });
+            state.isSetInterestRateModelLoading = false;
+            pendingStore.decrement();
+            return
+        } 
+        try {
+            await initInterestRateModelParams();
+            state.chartNeedRefresh = true;
+        } catch (error) {
+            console.error(error);
         }
     }
 
-    const initChart = (option: any) => {
-        const chartDom = document.getElementById("interest-model-chart");
-        const myChart = echarts.init(chartDom!);
-        myChart.setOption(option);
+    const setGatewayAddress = async () => {
+        state.currentGatewayAddress = state.inputGatewayAddress;
+    }
+
+    const setMinCollateralCoverageRatio = async () => {
+        let tx1, tx2, result1, result2;
+        try {
+            [tx1, tx2] = await Promise.all([
+                commonStore.getProtocol.setLoanAndCollateralTokenPair(
+                    commonStore.getTokens.SGC.address,
+                    commonStore.getTokens.ETH.address,
+                    ethers.utils.parseUnits(String(parseFloat(state.inputMinCollateralCoverageRatio) / 100), "ether"),
+                    ethers.utils.parseUnits("0.05", "ether")
+                ),
+                commonStore.getProtocol.setLoanAndCollateralTokenPair(
+                    commonStore.getTokens.SGC.address,
+                    commonStore.getTokens.xBTC.address,
+                    ethers.utils.parseUnits(String(parseFloat(state.inputMinCollateralCoverageRatio) / 100), "ether"),
+                    ethers.utils.parseUnits("0.05", "ether")
+                ),
+            ])
+        } catch (error) {
+            console.error(error);
+            pendingStore.enqueue({
+                title: "Configuration",
+                message: `Set Min CollateralCoverageRatio failed. (${utils.filterRevertMsg((error as any).message)})`,
+                color: "danger"
+            });
+            return;
+        }
+        try {
+            state.isSetMinCollateralCoverageRatioLoading = true;
+            pendingStore.increment();
+            [result1, result2] = await Promise.all([
+                tx1.wait(),
+                tx2.wait(),
+            ])
+            console.log("setMinCollateralCoverageRatio result1: ", result1);
+            console.log("setMinCollateralCoverageRatio result2: ", result2);
+            pendingStore.enqueue({
+                title: "Configuration",
+                message: `Set MinCollateralCoverageRatio ${state.inputMinCollateralCoverageRatio}% for collateral token pairs success.`,
+                color: "success"
+            });
+            state.isSetMinCollateralCoverageRatioLoading = false;
+            pendingStore.decrement();
+        } catch (error) {
+            console.error(error);
+            pendingStore.enqueue({
+                title: "Configuration",
+                message: `Set Min CollateralCoverageRatio failed. (${utils.filterRevertMsg((error as any).message)})`,
+                color: "danger"
+            });
+            state.isSetMinCollateralCoverageRatioLoading = false;
+            pendingStore.decrement();
+            return;
+        }
+        try {
+            await initCurrentMinCollateralRatio();
+        } catch(error) {
+            console.error(error);
+        }
+    }
+
+    async function initInterestRateModelParams() {
+        const rawParams = await commonStore.getInterestRateModel.getLoanParameters(commonStore.tokens.SGC.address);
+        const termList = rawParams.termList.map((term: BigNumber) => term.toNumber());
+        const interestRateList = rawParams.interestRateList.map((interestRate: BigNumber) => ethers.utils.formatEther(interestRate));
+        state.currentInterestRateParams = { termList, interestRateList };
+    }
+
+    async function initCurrentMinCollateralRatio() {
+        const filter = await commonStore.getProtocol.filters.SetLoanAndCollateralTokenPairSucceed();
+        const loanEvents = await commonStore.getProtocol.queryFilter(filter);
+        let ethSetted = false, xbtcSetted = false;
+        for (let i = loanEvents.length - 1; i >= 0; i--) {
+            if (!ethSetted && utils.getTokenNameFromAddress(loanEvents[i].args.collateralTokenAddress) === TokenType.ETH) {
+                state.currentMinCollateralCoverageRatio.ETH = 100 * parseFloat(ethers.utils.formatEther(loanEvents[i].args.minCollateralCoverageRatio));
+                ethSetted = true;
+            }
+            if (!xbtcSetted && utils.getTokenNameFromAddress(loanEvents[i].args.collateralTokenAddress) === TokenType.xBTC) {
+                state.currentMinCollateralCoverageRatio.xBTC = 100 * parseFloat(ethers.utils.formatEther(loanEvents[i].args.minCollateralCoverageRatio));
+                xbtcSetted = true;
+            }
+        };
     }
 
     return {
         state,
-        initChartOption,
         initChart,
+        setInterestRateParameters,
+        setGatewayAddress,
+        setMinCollateralCoverageRatio,
     }
 }
+
